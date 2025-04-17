eb4635559e439bc18a122300161d11b0c3b87624
Nikesh-3103 <nikeshkumar310305@gmail.com>
2025-04-17 20:28:42 +0530

    finally done

--- demo_release.py
+++ demo_release.py
@@ -16,9 +16,7 @@ colorizer_siggraph17 = siggraph17(pretrained=True).eval()
 if(opt.use_gpu):
 	colorizer_eccv16.cuda()
 	colorizer_siggraph17.cuda()
-
-# default size to process images is 256x256
-# grab L channel in both original ("orig") and resized ("rs") resolutions
+	
 img = load_img(opt.img_path)
 (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
 if(opt.use_gpu):
